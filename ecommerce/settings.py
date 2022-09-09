9170b913871d34e094147be83ace7b22738f0c45
Adolfo0018 <adolfogk0018@gmail.com>
2022-09-09 15:45:39 -0500

    'Se termino Sección 28: Django - Diseño de Variaciones en Ecommerce'

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accounts',
+    'carts',
     'category',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'category.context_processors.menu_links',
+                'carts.context_processors.counter',
             ],
         },
     },
